2d13d2c8b64b7c9a7d0772ae857d15583121303f
BobHathaway2 <hat22014@byui.edu>
2024-01-30 18:06:53 -0700

    authentication working?

--- routes/cars.js
+++ routes/cars.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const carsController = require('../controllers/cars.js');
 const validation = require('../middleware/validate');
+const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/:id', validation.checkMongoId, carsController.getSingle);
 router.get('/', carsController.getAll);
-router.post('/', validation.saveCar, carsController.createCar);
-router.put('/:id', validation.checkMongoId, validation.saveCar, carsController.updateCar);
-router.delete('/:id', validation.checkMongoId, carsController.deleteCar);
+router.post('/', isAuthenticated, validation.saveCar, carsController.createCar);
+router.put('/:id', isAuthenticated, validation.checkMongoId, validation.saveCar, carsController.updateCar);
+router.delete('/:id', isAuthenticated, validation.checkMongoId, carsController.deleteCar);
 
 module.exports = router;
--- routes/dogs.js
+++ routes/dogs.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const dogsController = require('../controllers/dogs.js');
 const validation = require('../middleware/validate');
+const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/:id', validation.checkMongoId, dogsController.getSingle);
 router.get('/', dogsController.getAll);
-router.post('/', validation.saveDog, dogsController.createDog);
-router.put('/:id', validation.checkMongoId, validation.saveDog, dogsController.updateDog);
-router.delete('/:id', validation.checkMongoId, dogsController.deleteDog);
+router.post('/', isAuthenticated, validation.saveDog, dogsController.createDog);
+router.put('/:id', isAuthenticated, validation.checkMongoId, validation.saveDog, dogsController.updateDog);
+router.delete('/:id', isAuthenticated, validation.checkMongoId, dogsController.deleteDog);
 
 module.exports = router;
